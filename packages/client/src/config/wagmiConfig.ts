4a420de9d7e330af86b2bb7bc15833ee2f8778ff
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-11-19 19:09:00 +0900

    update network

--- packages/client/src/config/wagmiConfig.ts
+++ packages/client/src/config/wagmiConfig.ts
@@ -1,18 +1,20 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig } from "wagmi";
 import {
-    optimismGoerli,
     polygonMumbai,
-    goerli
+    goerli,
+    avalancheFuji,
+    polygonZkEvmTestnet,
+    avalanche,
+    polygonZkEvm,
   } from 'wagmi/chains';
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-import { modeTestnet } from "./customChain";
 
 const { chains, publicClient } = configureChains(
   [
     ...(process.env.NODE_ENV === "development"
-      ? [optimismGoerli, polygonMumbai, goerli, modeTestnet]
-      : [optimismGoerli]),
+      ? [polygonMumbai, goerli, avalancheFuji, polygonZkEvmTestnet]
+      : [polygonMumbai, goerli, avalanche, polygonZkEvmTestnet, polygonZkEvm]),
   ],
   [
     jsonRpcProvider({
@@ -27,14 +29,19 @@ const { chains, publicClient } = configureChains(
             http: polygonMumbai.rpcUrls.public.http[0],
           };
 
-        if (chain.id === optimismGoerli.id)
+        if (chain.id === avalancheFuji.id)
           return {
-            http: optimismGoerli.rpcUrls.public.http[0],
+            http: avalancheFuji.rpcUrls.public.http[0],
           };
 
-        if (chain.id === modeTestnet.id)
+        if (chain.id === polygonZkEvmTestnet.id)
           return {
-            http: modeTestnet.rpcUrls.public.http[0],
+            http: polygonZkEvmTestnet.rpcUrls.public.http[0],
+          };
+
+        if (chain.id === polygonZkEvm.id)
+          return {
+            http: polygonZkEvm.rpcUrls.public.http[0],
           };
 
         return null;
